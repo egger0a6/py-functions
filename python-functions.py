e54a2564fc80d0dc8ab30a0b0c541acbe5fc37a7
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-29 21:36:27 -0400

    remove comments

--- python-functions.py
+++ python-functions.py
@@ -13,16 +13,7 @@ def sum_to(n):
 
 
 def largest(numList):
-  '''-----------------------------------------------------------------------------
-  #  function largest
-  #
-  #  purpose: recursive function that accepts a list of integers and returns the
-  #   largest number in that list
-  #
-  #  @param numList : list of integers
-  #
-  #  returns: int
-  -----------------------------------------------------------------------------'''
+
   def largest_helper(numList, max):
     if numList[-1] > max: max = numList[-1]
     if len(numList) <= 1: return max
@@ -33,17 +24,7 @@ def largest(numList):
 
 
 def occurences(str1, str2):
-  '''-----------------------------------------------------------------------------
-  #  function occurences
-  #
-  #  purpose: recursive function that takes 2 string arguments and counts the 
-  #   number of occurences of the second string inside the first
-  #
-  #  @param str1 : string
-  #  @param str2 : string
-  #
-  #  returns: int
-  -----------------------------------------------------------------------------'''
+
   def occurance_count(str1, str2):
     if (str1.find(str2) == -1 or len(str1) < len(str2)): return 0
     index = str1.find(str2)
@@ -53,16 +34,7 @@ def occurences(str1, str2):
 
 
 def product(*nums):
-  '''-----------------------------------------------------------------------------
-  #  function product
-  #
-  #  purpose: recursive function that accepts arbitrary amount of numbers and 
-  #   returns the product of them all multiplied together
-  #
-  #  @param *nums : arbitrary number of numbers
-  #
-  #  returns: int or float
-  -----------------------------------------------------------------------------'''
+  
   def product_with_index(nums, idx):
     if (idx == len(nums)): return 1
     return nums[idx] * product_with_index(nums, idx+1)
